723a11b9992df8dc6cc7e18b8943c06f9996a012
Sachin Kiran Shah <sachinksha@gmail.com>
2026-06-17 23:04:27 +0530

    added code for ts specific problems for tuple length and figuring out type of first element of the tuple. also fixed runtime error for depricated module.register() due to old version of tsc.

--- src/problems/05-throttle/throttle.vanila.ts
+++ src/problems/05-throttle/throttle.vanila.ts
@@ -1,11 +1,20 @@
 // bun test src/problems/05-throttle/test/throttle.test.ts
 
-export function throttle() {
-
+export function throttle<F extends (...args: any[]) => void>(
+  fn: F,
+  delay: number,
+): (...args: Parameters<F>) => void {
+  let lastTime:number = 0;
+  return function throttled(this: unknown, ...args: Parameters<F>): void {
+    const currentTime:number = Date.now();
+    if(currentTime - lastTime >= delay) {
+      fn.apply(this, args);
+      lastTime = currentTime;
+    }
+  }
 }
 // --- Examples ---
 // Uncomment to test your implementation:
-
 // const log = throttle((msg: string) => console.log(msg), 300)
 // log('a')  // fires immediately → "a"
 // log('b')  // ignored (within 300ms)
